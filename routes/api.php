acdcb6e776e12dd92f34d68645ceb3ca81a01f08
Ali Ibrahem <en_ali_8@hotmail.com>
2020-06-01 06:45:37 +0200

    Removed unnecessary routes

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('v1/flights', v1\FlightController::class);
+Route::resource('v1/flights', v1\FlightController::class , [
+    'except' => ['create' , 'edit']
+]);
